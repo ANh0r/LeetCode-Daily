fb2feda9c33b4f701f8d3dae88ca6f0736c6adc6
ANh0r <819702016@qq.com>
2020-09-14 08:37:03 +0800

    Traverse Bitree

---
+++ 9.14 InorderTraverseBitree.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        stack = []
+        cur = root
+        # 中序，模板：先用指针找到每颗子树的最左下角，然后进行进出栈操作
+        while stack or cur:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            res.append(cur.val)
+            cur = cur.right
+        return res
+    
+"""
+输入: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+输出: [1,3,2]
+"""
