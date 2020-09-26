f44c528575d6e46860b26117bb95567d99d172ed
ANh0r <819702016@qq.com>
2020-09-26 16:18:09 +0800

    DFS sumofBTreturnpath

---
+++ 9.26 SumBT.py
@@ -0,0 +1,56 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def pathSum(self, root: TreeNode, total: int) -> List[List[int]]:
+        ret = list()
+        path = list()
+
+        def dfs(root: TreeNode, total: int):
+            if not root:
+                return
+            path.append(root.val)
+            total -= root.val
+            if not root.left and not root.right and total == 0:
+                ret.append(path[:])
+            dfs(root.left, total)
+            dfs(root.right, total)
+            path.pop()
+
+        dfs(root, total)
+        return ret
+
+
+    """
+    枚举每一条从根节点到叶子节点的路径。当我们遍历到叶子节点，且此时路径和恰为目标和时，我们就找到了一条满足条件的路径。
+    """
+"""
+给定一个二叉树和一个目标和，找到所有从根节点到叶子节点路径总和等于给定目标和的路径。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例:
+给定如下二叉树，以及目标和 sum = 22，
+
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \    / \
+        7    2  5   1
+返回:
+
+[
+   [5,4,11,2],
+   [5,8,4,5]
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/path-sum-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。"""
