8df417fc835b9f383aa9ccee5613715957034821
ANh0r <819702016@qq.com>
2020-09-27 09:07:14 +0800

    ancestor BT

---
+++ 9.27 ancestorBT.py
@@ -0,0 +1,50 @@
+
+"""
+给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+
+百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。
+
+”
+输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+输出: 6
+解释: 节点 2 和节点 8 的最近公共祖先是 6。
+"""
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        ancestor = root
+        while True:
+            if p.val < ancestor.val and q.val < ancestor.val:
+                ancestor = ancestor.left
+            elif p.val > ancestor.val and q.val > ancestor.val:
+                ancestor = ancestor.right
+            else:
+                break
+        return ancestor
+    '''
+        while(root):
+            if p.val<root.val and q.val<root.val:
+                root=root.left
+            elif p.val>root.val and q.val>root.val:
+                root=root.right
+            else:
+                return root'''
+
+"""
+思路与算法
+
+在方法一中，我们对从根节点开始，通过遍历找出到达节点 pp 和 qq 的路径，一共需要两次遍历。我们也可以考虑将这两个节点放在一起遍历。
+
+整体的遍历过程与方法一中的类似：
+
+我们从根节点开始遍历；
+
+如果当前节点的值大于 p 和 q 的值，说明 p 和 q 应该在当前节点的左子树，因此将当前节点移动到它的左子节点；
+
+如果当前节点的值小于 p 和 q 的值，说明 p 和 q 应该在当前节点的右子树，因此将当前节点移动到它的右子节点；
+
+如果当前节点的值不满足上述两条要求，那么说明当前节点就是「分岔点」。此时，pp 和 qq 要么在当前节点的不同的子树中，要么其中一个就是当前节点。
+
+可以发现，如果我们将这两个节点放在一起遍历，我们就省去了存储路径需要的空间。
+"""
