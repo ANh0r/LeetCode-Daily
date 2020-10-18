70a7500f2ef457a72fdf539c2551725bf61b1af1
ANh0r <819702016@qq.com>
2020-10-18 09:31:32 +0800

    10.18 NthFromEndRomove

---
+++ 10.18 removeNthFromEnd.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        dummy = ListNode(0, head)
+        first = head
+        second = dummy
+        for i in range(n):
+            first = first.next
+
+        while first:
+            first = first.next
+            second = second.next
+
+        second.next = second.next.next
+        return dummy.next
+
+
+"""快慢指针"""
