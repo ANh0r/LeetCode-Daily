48e2ad9492232f9470448b1a77ff44dd60cf8b1a
ANh0r <819702016@qq.com>
2020-10-22 11:06:37 +0800

    10.22 partition

---
+++ 10.22 partitionLables.py
@@ -0,0 +1,30 @@
+class Solution:
+    def partitionLabels(self, S: str) -> List[int]:
+        last = [0] * 26
+        for i, ch in enumerate(S):
+            last[ord(ch) - ord("a")] = i
+
+        partition = list()
+        start = end = 0
+        for i, ch in enumerate(S):
+            end = max(end, last[ord(ch) - ord("a")])
+            if i == end:
+                partition.append(end - start + 1)
+                start = end + 1
+
+        return partition
+
+"""
+字符串 S 由小写字母组成。我们要把这个字符串划分为尽可能多的片段，同一个字母只会出现在其中的一个片段。返回一个表示每个字符串片段的长度的列表。
+
+ 
+
+示例 1：
+
+输入：S = "ababcbacadefegdehijhklij"
+输出：[9,7,8]
+解释：
+划分结果为 "ababcbaca", "defegde", "hijhklij"。
+每个字母最多出现在一个片段中。
+像 "ababcbacadefegde", "hijhklij" 的划分是错误的，因为划分的片段数较少。
+。"""
