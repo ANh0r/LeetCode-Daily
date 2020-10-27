36e239a769c7284b574b6b36c006a044c6d11f5e
ANh0r <819702016@qq.com>
2020-10-27 08:29:23 +0800

    10.27 preorderTraverse BT

---
+++ 10.27 preorderTraverse.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return[]
+        res = []
+        stack = [root]
+        while stack:
+            cur = stack.pop()
+            res.append(cur.val)
+            if cur.right:
+                stack.append(cur.right)
+            if cur.left:
+                stack.append(cur.left)
+        return res# 索然无味
