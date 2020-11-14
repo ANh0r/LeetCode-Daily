a82bd8a2234563f4a7d4a4464641d0dc261ec419
ANh0r <819702016@qq.com>
2020-11-14 10:57:12 +0800

    11.14 ArrayRelativeSort

--- 11.13 evenOddlist.py
+++ 11.13 evenOddlist.py
@@ -3,15 +3,18 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+
 class Solution:
-    def oddEvenList(self, head: ListNode) -> ListNode:
-       if head == None: return head
-       point1, point2 = head, head.next
-       p1, p2 = point1, point2
-       while p2 != None and p2.next:
-           p1.next = p1.next.next
-           p2.next = p2.next.next
-           p1 = p1.next
-           p2 = p2.next
-       p1.next = point2
-       return point1
+        def oddEvenList(self, head: ListNode) -> ListNode:
+            if head == None:
+                   return head
+               point1, point2 = head, head.next
+               p1, p2 = point1, point2
+               while p2 != None and p2.next:
+                   p1.next = p1.next.next
+                   p2.next = p2.next.next
+                   p1 = p1.next
+                   p2 = p2.next
+               p1.next = point2
+               return point1
