ccb412a74e2ac4b2753875ed2a68a4875433fc08
ANh0r <819702016@qq.com>
2020-11-16 09:41:47 +0800

    11.15 & 11.16

---
+++ 11.15 RemoveKdigits.py
@@ -0,0 +1,41 @@
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        numStack = []
+
+        # 构建单调递增的数字串
+        for digit in num:
+            while k and numStack and numStack[-1] > digit:
+                numStack.pop()
+                k -= 1
+
+            numStack.append(digit)
+
+        # 如果 K > 0，删除末尾的 K 个字符
+        finalStack = numStack[:-k] if k else numStack
+
+        # 抹去前导零
+        return "".join(finalStack).lstrip('0') or "0"
+
+    """
+    给定一个以字符串表示的非负整数 num，移除这个数中的 k 位数字，使得剩下的数字最小。
+
+注意:
+
+num 的长度小于 10002 且 ≥ k。
+num 不会包含任何前导零。
+示例 1 :
+
+输入: num = "1432219", k = 3
+输出: "1219"
+解释: 移除掉三个数字 4, 3, 和 2 形成一个新的最小的数字 1219。
+示例 2 :
+
+输入: num = "10200", k = 1
+输出: "200"
+解释: 移掉首位的 1 剩下的数字为 200. 注意输出不能有任何前导零。
+示例 3 :
+
+输入: num = "10", k = 2
+输出: "0"
+解释: 从原数字移除所有的数字，剩余为空就是0。
+。"""
---
+++ 11.16 reConstructQueue.py
@@ -0,0 +1,37 @@
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        people.sort(key=lambda x: (x[0], -x[1]))
+        n = len(people)
+        ans = [[] for _ in range(n)]
+        for person in people:
+            spaces = person[1] + 1
+            for i in range(n):
+                if not ans[i]:
+                    spaces -= 1
+                    if spaces == 0:
+                        ans[i] = person
+                        break
+        return ans
+    """
+    people.sort(key=lambda p : (-p[0], p[1]))
+    res = []
+    for p in people:
+        res.insert(p[1], p)
+    return res
+    """
+    """
+    假设有打乱顺序的一群人站成一个队列。 每个人由一个整数对(h, k)表示，其中h是这个人的身高，k是排在这个人前面且身高大于或等于h的人数。 编写一个算法来重建这个队列。
+
+注意：
+总人数少于1100人。
+
+示例
+
+输入:
+[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+
+输出:
+[[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+
+
+"""
