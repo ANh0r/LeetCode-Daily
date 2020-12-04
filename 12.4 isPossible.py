76661560403d4b8d38fd257315fff1dbab78a7ca
ANh0r <819702016@qq.com>
2020-12-04 10:28:44 +0800

    12.4 isPossible

---
+++ 12.4 isPossible.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPossible(self, nums: List[int]) -> bool:
+        mp = collections.defaultdict(list)
+        for x in nums:
+            if queue := mp.get(x - 1):
+                prevLength = heapq.heappop(queue)
+                heapq.heappush(mp[x], prevLength + 1)
+            else:
+                heapq.heappush(mp[x], 1)
+
+        return not any(queue and queue[0] < 3 for queue in mp.values())
