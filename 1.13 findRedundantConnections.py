d36bc350145649d6d0b873d550bffb83b7d5a217
ANh0r <819702016@qq.com>
2021-01-13 00:13:19 +0800

    1.13 findReduntdantConnections

---
+++ 1.13 findRedundantConnections.py
@@ -0,0 +1,39 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        nodesCount = len(edges)
+        parent = list(range(nodesCount + 1))
+
+        def find(index: int) -> int:
+            if parent[index] != index:
+                parent[index] = find(parent[index])
+            return parent[index]
+
+        def union(index1: int, index2: int):
+            parent[find(index1)] = find(index2)
+
+        for node1, node2 in edges:
+            if find(node1) != find(node2):
+                union(node1, node2)
+            else:
+                return [node1, node2]
+
+        return []
+"""
+在本问题中, 树指的是一个连通且无环的无向图。
+
+输入一个图，该图由一个有着N个节点 (节点值不重复1, 2, ..., N) 的树及一条附加的边构成。附加的边的两个顶点包含在1到N中间，这条附加的边不属于树中已存在的边。
+
+结果图是一个以边组成的二维数组。每一个边的元素是一对[u, v] ，满足 u < v，表示连接顶点u 和v的无向图的边。
+
+返回一条可以删去的边，使得结果图是一个有着N个节点的树。如果有多个答案，则返回二维数组中最后出现的边。答案边 [u, v] 应满足相同的格式 u < v。
+
+示例 1：
+
+输入: [[1,2], [1,3], [2,3]]
+输出: [2,3]
+解释: 给定的无向图为:
+  1
+ / \
+2 - 3
+
+"""
