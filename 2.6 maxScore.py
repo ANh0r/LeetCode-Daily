bfcff78b0570a746f8edc0dbd4ef260c4ec4161d
ANh0r <819702016@qq.com>
2021-02-06 10:52:41 +0800

    2.6 maxScore

---
+++ 2.6 maxScore.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        n = len(cardPoints)
+        # 滑动窗口大小为 n-k
+        windowSize = n - k
+        # 选前 n-k 个作为初始值
+        s = sum(cardPoints[:windowSize])
+        minSum = s
+        for i in range(windowSize, n):
+            # 滑动窗口每向右移动一格，增加从右侧进入窗口的元素值，并减少从左侧离开窗口的元素值
+            s += cardPoints[i] - cardPoints[i - windowSize]
+            minSum = min(minSum, s)
+        return sum(cardPoints) - minSum
