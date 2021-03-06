14cf44fe8da0d701fcd1c90bdd818d15a27357b7
ANh0r <819702016@qq.com>
2021-03-06 09:45:09 +0800

    3.6 NextGreaterElements

---
+++ 3.6 NextGreaterElements.py
@@ -0,0 +1,12 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        ret = [-1] * n
+        stk = list()
+
+        for i in range(n * 2 - 1):
+            while stk and nums[stk[-1]] < nums[i % n]:
+                ret[stk.pop()] = nums[i % n]
+            stk.append(i % n)
+
+        return ret
