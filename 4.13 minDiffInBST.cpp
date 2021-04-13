089b51dc44ef7942f27ccab921bc10c2cddf222c
ANh0r <819702016@qq.com>
2021-04-13 08:27:00 +0800

    4.13 minDiffInBST Need Review

---
+++ 4.13 minDiffInBST.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void dfs(TreeNode* root, int& pre, int& ans) {
+        if (root == nullptr) {
+            return;
+        }
+        dfs(root->left, pre, ans);
+        if (pre == -1) {
+            pre = root->val;
+        } else {
+            ans = min(ans, root->val - pre);
+            pre = root->val;
+        }
+        dfs(root->right, pre, ans);
+    }
+    int minDiffInBST(TreeNode* root) {
+        int ans = INT_MAX, pre = -1;
+        dfs(root, pre, ans);
+        return ans;
+    }
+};
