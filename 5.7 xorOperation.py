6091c0fe6f4fca61084922bd1209346bf9afcbd6
ANh0r <819702016@qq.com>
2021-05-07 07:49:29 +0800

    5.7 xorOperation

---
+++ 5.7 xorOperation.py
@@ -0,0 +1,7 @@
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        strs = [start + 2*i for i in range(0,n)]
+        sum = 0
+        for i in range (0,len(strs)):
+            sum ^= strs[i]
+        return sum
