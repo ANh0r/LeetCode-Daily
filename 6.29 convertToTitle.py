ab1011b4402f329dd4dca20c1fa286fab317e822
ANh0r <819702016@qq.com>
2021-06-29 07:19:10 +0800

    6.29 convertToTitle

---
+++ 6.29 convertToTitle.py
@@ -0,0 +1,8 @@
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        ans = list()
+        while columnNumber > 0:
+            columnNumber -= 1
+            ans.append(chr(columnNumber % 26 + ord("A")))
+            columnNumber //= 26
+        return "".join(ans[::-1])
