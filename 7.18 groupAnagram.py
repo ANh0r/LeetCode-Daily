0bffaaed894e9116bc8324188e6fd89ba95e9ef1
ANh0r <819702016@qq.com>
2021-07-18 10:39:45 +0800

    7.18 groupAnagram

---
+++ 7.18 groupAnagram.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mp = collections.defaultdict(list)
+
+        for st in strs:
+            key = "".join(sorted(st))
+            mp[key].append(st)
+
+        return list(mp.values())
