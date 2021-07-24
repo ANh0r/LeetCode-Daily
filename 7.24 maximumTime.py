9d533c1f88ccfa14fef20356968585dc301167b9
ANh0r <819702016@qq.com>
2021-07-24 08:41:22 +0800

    7.24 maximumTime

---
+++ 7.24 maximumTime.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maximumTime(self, time: str) -> str:
+        [a, b, _, c, d] = time
+        if a == '?':
+            a = '2' if b == '?' or b < '4' else '1'
+        if b == '?':
+            b = '3' if a == '2' else '9'
+        if c == '?':
+            c = '5'
+        if d == '?':
+            d = '9'
+        return f'{a}{b}:{c}{d}'
