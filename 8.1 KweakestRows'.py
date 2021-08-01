c974049558fb26dde8ce57c4ebf47d53c33e5cc4
ANh0r <819702016@qq.com>
2021-08-01 08:39:50 +0800

    8.1 kWeakestRows

---
+++ 8.1 KweakestRows'.py
@@ -0,0 +1,20 @@
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        m, n = len(mat), len(mat[0])
+        power = list()
+        for i in range(m):
+            l, r, pos = 0, n - 1, -1
+            while l <= r:
+                mid = (l + r) // 2
+                if mat[i][mid] == 0:
+                    r = mid - 1
+                else:
+                    pos = mid
+                    l = mid + 1
+            power.append((pos + 1, i))
+
+        heapq.heapify(power)
+        ans = list()
+        for i in range(k):
+            ans.append(heapq.heappop(power)[1])
+        return ans
