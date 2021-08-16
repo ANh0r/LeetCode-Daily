90b9ebeb09662e46ceb6cbdbf26c9411a56f0e1e
ANh0r <819702016@qq.com>
2021-08-16 08:31:12 +0800

    8.16 countArrangement

---
+++ 8.16 countArrangements.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countArrangement(self, n: int) -> int:
+        match = defaultdict(list)
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                if i % j == 0 or j % i == 0:
+                    match[i].append(j)
+
+        num = 0
+        vis = set()
+
+        def backtrack(index: int) -> None:
+            if index == n + 1:
+                nonlocal num
+                num += 1
+                return
+
+            for x in match[index]:
+                if x not in vis:
+                    vis.add(x)
+                    backtrack(index + 1)
+                    vis.discard(x)
+
+        backtrack(1)
+        return num
