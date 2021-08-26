2406650e0cacb13e7d3adbb820f5598247e6cb0c
ANh0r <819702016@qq.com>
2021-08-26 13:53:29 +0800

    8.26 numRescueBoats

---
+++ 8.26 numRescueBoats.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:dist = {}
+        for num in people:
+            if num not in dist:
+                dist[num] = 1
+            else:
+                dist[num] += 1
+        res = 0
+        for i in range(len(people)-1, -1, -1):
+            a = people[i]
+            if dist[a] > 0:
+                dist[a] -= 1
+            else:
+                continue
+            b = limit - a
+            for j in range(b, -1 ,-1):
+                if j in dist and dist[j] > 0:
+                    dist[j] -= 1
+                    break
+            res += 1
+        return res
