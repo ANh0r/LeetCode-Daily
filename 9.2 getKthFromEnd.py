00442f2b6c91c8372298f4dc17fc69a2e3efc3c2
ANh0r <819702016@qq.com>
2021-09-02 06:50:11 +0800

    9.2 getKthFromEnd

---
+++ 9.2 getKthFromEnd.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getKthFromEnd(self, head: ListNode, k: int) -> ListNode:
+        fast, slow = head, head
+
+        while fast and k > 0:
+            fast, k = fast.next, k - 1
+        while fast:
+            fast, slow = fast.next, slow.next
+
+        return slow
