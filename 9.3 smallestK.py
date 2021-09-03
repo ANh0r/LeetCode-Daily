5a04e94f07cc4a29a60ca8a118195333aff33867
ANh0r <819702016@qq.com>
2021-09-03 08:17:20 +0800

    9.2 getKthFromEnd

---
+++ 9.3 smallestK.py
@@ -0,0 +1,22 @@
+class Solution:
+    def smallestK(self, arr: List[int], k: int) -> List[int]:
+        def qs(left,right):
+            if left>=right:
+                return
+            pivot,i,j = arr[left],left,right
+            while i<j:
+                while i<j and arr[j]>=pivot:
+                    j-=1
+                arr[i] = arr[j]
+                while i<j and arr[i]<=pivot:
+                    i+=1
+                arr[j] = arr[i]
+            arr[i] =  pivot
+            if i==k-1:
+                return
+            elif i<k-1:
+                qs(i+1,right)
+            else:
+                qs(left,i-1)
+        qs(0,len(arr)-1)
+        return arr[:k]
